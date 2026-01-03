ea53b888b23f29a1fbb26efe53240a8bc8c8e54d
oxyethylene <ljunyfor@outlook.com>
2026-01-03 18:09:04 +0800

    feat: init project

---
+++ src/main/java/com/github/oxyethylene/otelcollector/OtelCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.github.oxyethylene.otelcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelCollectorApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(OtelCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/oxyethylene/otelcollector/OtelCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oxyethylene.otelcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
